a6a3f257a75505496fd729a9678efd45262dd6c8
Rustam Kalimulin <r.kalimulin@notamedia.ru>
2022-09-11 19:08:02 +0400

    получение данных от api

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="weather-widget" v-cloak>
+    <Widget msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Widget from './components/Widget.vue'
+import configJson from './config.json'
+import { openWeatherSearch } from './services/OpenWeatherAPI'
+import { WeatherData } from './types/WeaterDataTypes.interface'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Widget
+  },
+
+  data: () => {
+    return {
+      configData: configJson,
+      apiKey: '',
+      weatherData: {} as WeatherData
+    }
+  },
+
+  created () {
+    console.log(this.configData)
+    if (this.configData.apiKey) {
+      this.apiKey = this.configData.apiKey
+    }
+    this.getWeather('55.7522', '37.6156', '63836fc048c107a45c0d642aceb98ec8')
+  },
+
+  methods: {
+    async getWeather (lat: string, lon: string, apiKey: string): Promise<void> {
+      const value = await openWeatherSearch(lat, lon, apiKey)
+      console.log(value)
+    }
   }
 })
 </script>
 
 <style lang="scss">
-#app {
+.weather-widget {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const widgets = document.getElementsByTagName('weather-widget') as HTMLCollectionOf<HTMLElement>
+for (const widget of widgets) {
+  widget.classList.add('vue-app-mounted')
+  createApp(App).mount(widget)
+}
